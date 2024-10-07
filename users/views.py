fa50929bb9e91f6a20f7a0792d69da6982d65d5f
Adil <anwaradil295@gmail.com>
2024-10-07 20:31:36 +0530

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .froms import UserRegisterForm
+# Create your views here.
+# urlpatterns = [
+#     path('')
+# ]
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f"Account Created for {username}!")
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request,'users/register.html',{'form':form})
